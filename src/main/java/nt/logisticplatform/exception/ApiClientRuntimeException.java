375bfe9238bfc98ae745647c2f2383259287bf42
Nikolay Todorov <nikolayet99@gmail.com>
2024-02-11 13:52:44 +0200

    Custom exception and global exception handler

---
+++ src/main/java/nt/logisticplatform/exception/ApiClientRuntimeException.java
@@ -0,0 +1,7 @@
+package nt.logisticplatform.exception;
+
+public class ApiClientRuntimeException extends RuntimeException {
+    public ApiClientRuntimeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/nt/logisticplatform/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package nt.logisticplatform.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ApiClientRuntimeException.class)
+    public ResponseEntity<String> handleApiClientRuntimeException(ApiClientRuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
